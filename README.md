66bcc6c2c110bee0c4a847cda6baefe9bd4ca3ad
Søren Rasmussen <brokzftw@gmail.com>
2012-10-25 21:35:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# currency.js
+
+## Installation
+
+```npm install currency -g```
+
+## Usage
+```
+currency 10 USD DKK
+=> 57.54916
+```
+
+## LICENSE
+  DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                   Version 2, December 2004
+
+Copyright (C) 2012 Søren Brokær <root@srn.io>
+
+Everyone is permitted to copy and distribute verbatim or modified
+copies of this license document, and changing it is allowed as long
+as the name is changed.
+
+           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+ 0. You just DO WHAT THE FUCK YOU WANT TO.
---
+++ currency.js
@@ -0,0 +1,40 @@
+var requirejs = require('requirejs').config({nodeRequire:require, baseUrl:''});
+
+requirejs(['http', 'commander', 'money'], function (http, commander, money) {
+  var options = {
+    host: 'openexchangerates.org',
+    path: '/api/latest.json?app_id=89d56115f20d40d7a898ee00e282a04f'
+  };
+
+  var data = '';
+  http.request(options, function(response){
+    response.on('data', function (chunk) {
+      data += chunk;
+    });
+
+    response.on('end', callback);
+  }).end();
+
+  var callback = function(){
+    data = JSON.parse(data);
+    money.base = data.rates;
+    money.rates = data.rates;
+
+    commander
+        .version('0.0.1');
+
+    commander
+        .command('* <currency> * [from] * [to]')
+        .description('convert from any currency to any currency')
+        .action(function(currency, from, to){
+          from = from ? from.toUpperCase() : 'USD';
+          to = to ? to.toUpperCase() : 'DKK';
+
+          var converted = money.convert(currency, {from: from, to: to});
+
+          console.log('=>', converted);
+        });
+
+    commander.parse(process.argv);
+  };
+});
