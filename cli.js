2adbb762a2c7e0b723258c1b29eb66a69daa1432
Matias Singers <mail@matiassingers.com>
2014-09-18 22:23:51 +0800

    Write new cli.js, and remove old bin/currency

---
+++ cli.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+'use strict';
+
+var pkg = require('./package.json');
+var currency = require('./');
+var argv = process.argv.slice(2);
+
+function help() {
+  console.log([
+    '',
+      '  ' + pkg.description,
+    '',
+    '  Example',
+    '    currency 10 USD DKK',
+    '',
+      '    => 57.75516'
+  ].join('\n'));
+}
+
+if (argv.indexOf('--help') !== -1) {
+  help();
+  return;
+}
+
+if (argv.indexOf('--version') !== -1) {
+  console.log(pkg.version);
+  return;
+}
+
+currency(argv[0], argv[1], argv[2], function(converted){
+  console.log(converted);
+});
