b2e99570d7eb6e250728fdca3162366572080042
Søren Brokær <root@srn.io>
2015-07-24 03:30:02 +0700

    default amount to `10` if not set

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var appId = process.env.APP_ID || '9c3a9fa4d4b7415e95880e677db3d080';
 module.exports = function (amount, from, to, callback) {
   var path = 'https://openexchangerates.org/api/latest.json?app_id=' + appId;
 
+  amount = amount || 10;
   from = from ? from.toUpperCase() : 'USD';
   to = to ? to.toUpperCase() : 'DKK';
 
